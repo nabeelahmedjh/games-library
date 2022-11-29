df8134cfebe8bb2eb1a920db1e49f2b7c544d690
nabeelahmedjh <abdulnabeel40@gmail.com>
2022-11-29 11:39:08 +0500

    added CRUD functionality in dashboard route

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ cur = conn.cursor()
 
 @app.route('/')
 def home():
+
     qry = 'SELECT * FROM GLP.game'
     cur.execute(qry)
     data = cur.fetchall()
@@ -27,12 +28,64 @@ def home():
     }
     return render_template("index.html", context=context)
 
+@app.route('/add-game', methods=['POST', 'GET'])
+def addGame():
+    if request.method == 'GET':
+        return render_template('add_game.html')
+    elif request.method == 'POST':
+        game_name= request.form['game_name']
+        print(game_name)
+        qry = "INSERT INTO GLP.game (game_title) VALUES (:1)"
+        cur.execute(qry, (game_name,))
+        conn.commit()
+        return redirect('/')
+        # return redirect(url_for('/'))
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+def dashboard():
+    if request.method == 'GET':
+        qry = 'SELECT * FROM GLP.game ORDER BY add_at DESC'
+        cur.execute(qry)
+        data = cur.fetchall()
+        context = {
+            'data': data
+        }
+        return render_template('dashboard.html', context=context)
+
+
+@app.route('/delete-game/<int:pk>', methods=['GET', 'POST'])
+def deleteGame(pk):
+    if request.method == 'GET':
+        return render_template('delete_game.html')
+    elif request.method == 'POST':
+        qry = 'DELETE FROM GLP.game WHERE game_id = :1'
+        cur.execute(qry, [pk])
+        conn.commit()
+        return redirect('/dashboard')
+
+@app.route('/edit-game/<int:pk>', methods=['GET', 'POST'])
+def editGame(pk):
+    if request.method == 'GET':
+        qry = 'SELECT * FROM GLP.game WHERE game_id=:1'
+        cur.execute(qry, [pk])
+        data = cur.fetchall()
+        context = {
+            "data": data
+        }
+        return render_template('edit_game.html', context=context)
+    elif request.method == 'POST':
+        qry = "UPDATE GLP.game SET game_title = :1 WHERE game_id = :2"
+        updated_data = request.form
+        game_title = updated_data['game_title'].lower()
+        
+        cur.execute(qry, [game_title, pk])
+        conn.commit()
+        return redirect('/dashboard')
 
 # conn.commit()
 
 # cur.close()
 # conn.close()
-
 
 
 if __name__ == '__main__':
