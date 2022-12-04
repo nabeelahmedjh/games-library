529a15f55f95c471f53ef65680575dd2afc1a557
nabeelahmedjh <abdulnabeel40@gmail.com>
2022-12-04 11:36:39 +0500

    added authorization and authenication routes

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import cx_Oracle
-from flask import Flask, redirect, url_for, request, render_template
-
-
+from flask import Flask,flash, redirect, url_for, request, render_template, session, g
+import base64
+from flask_uploads import UploadSet, configure_uploads
+from flask_httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+auth = HTTPBasicAuth()
 
 # connection string in the format
 # <username>/<password>@<dbHostAddress>:<dbPort>/<dbserviceName>
@@ -17,6 +21,74 @@ conn = cx_Oracle.connect(connectionString)
 cur = conn.cursor()
 
 
+ 
+ 
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == "GET":
+        return render_template('register.html')
+    else:
+        username = request.form['username']
+        password = request.form['password1']
+        confirm_password = request.form['password2']
+        
+        if username and password:
+            if password != confirm_password:
+                flash("password doesn't match in password and confirm passowrd field")
+                return redirect('/register')
+
+            qry = 'SELECT * FROM GLP.users WHERE username= :1'
+            
+            cur.execute(qry, [username])
+            
+            found = cur.fetchall()
+            
+            if len(found) != 0:
+                flash("User already exist")
+                return redirect('/register')
+            
+            qry = 'INSERT INTO GLP.users (username, password) VALUES (:1, :2)'
+            
+            cur.execute(qry, [username, password])
+            conn.commit()
+            flash("Sucessfully registered")
+            return redirect('/')
+        flash("Username or password doesn't exist")
+        return redirect('/register')
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        session['user'] = None
+        return render_template('login.html')
+    else:
+        
+        username = request.form['username']
+        password = request.form['password1']
+        qry = 'SELECT username, password FROM GLP.users WHERE username= :1'
+        
+        cur.execute(qry, [username])
+        
+        user = cur.fetchall()
+        print(user)
+        if len(user) == 0:
+            flash("USER DOESN'T EXIST")
+            return redirect(url_for("login"))
+        
+        if password != user[0][1]:
+            flash("provided password is wrong")
+            return redirect(url_for("login"))
+
+        session['user'] = username
+        return redirect(url_for("home"))
+
+@app.route('/logout')
+def logout():
+    session['user'] =  None
+    return redirect(url_for('home'))
+        
 @app.route('/')
 def home():
 
@@ -30,7 +102,12 @@ def home():
 
 @app.route('/add-game', methods=['POST', 'GET'])
 def addGame():
+    
+
+        
     if request.method == 'GET':
+        # if 'user' not in session or session['user'] == None:
+        return redirect(url_for('login'))
         return render_template('add_game.html')
     elif request.method == 'POST':
         game_name= request.form['game_name']
@@ -38,18 +115,51 @@ def addGame():
         qry = "INSERT INTO GLP.game (game_title) VALUES (:1)"
         cur.execute(qry, (game_name,))
         conn.commit()
-        return redirect('/')
+        return redirect('/upload')
         # return redirect(url_for('/'))
+
+@app.route('/upload', methods=['POST', 'GET'])
+def upload():
+        
+    if 'user' not in session:
+        return redirect(url_for('login'))
+
+
+    if request.method == 'POST':
+        file = request.files['inputFile']
+        data = file.read()
+        render_file = render_picture(data)
+        name = request.form['name']
+        print(render_file)
+        qry = "INSERT INTO GLP.game (game_title, game_cover) VALUES (:1, :2)"
+        cur.execute(qry, [name, render_file])
+        conn.commit()
+        return f"success"
+        return render_template('upload.html')
+    else:
+        return render_template('upload_game_image.html')
+
+@app.route('/add-requirements/<int:pk>')
+
 
 @app.route('/dashboard', methods=['GET', 'POST'])
 def dashboard():
+    
+    if 'user' not in session or session['user'] == None:
+        flash("LOGIN REQUIRED FOR VIEWING DASHBOARD")
+        return redirect(url_for('login'))
+    
     if request.method == 'GET':
+        
         qry = 'SELECT * FROM GLP.game ORDER BY add_at DESC'
         cur.execute(qry)
         data = cur.fetchall()
         context = {
             'data': data
         }
+        for game in context['data']:
+            print(game)
+            
         return render_template('dashboard.html', context=context)
 
 
@@ -62,6 +172,7 @@ def deleteGame(pk):
         cur.execute(qry, [pk])
         conn.commit()
         return redirect('/dashboard')
+
 
 @app.route('/edit-game/<int:pk>', methods=['GET', 'POST'])
 def editGame(pk):
@@ -82,6 +193,11 @@ def editGame(pk):
         conn.commit()
         return redirect('/dashboard')
 
+
+def render_picture(data):
+
+    render_pic = base64.b64encode(data).decode('ascii') 
+    return render_pic
 # conn.commit()
 
 # cur.close()
@@ -89,4 +205,5 @@ def editGame(pk):
 
 
 if __name__ == '__main__':
+    app.run(debug=True)
     app.run()
